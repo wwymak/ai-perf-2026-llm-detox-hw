5ed28369701677a38e474f1038916a2da68fad3d
wendy mak <wendy.mak@stepstone.com>
2026-06-29 11:39:40 +0100

    wip

--- tasks/task8_custom_reward.py
+++ tasks/task8_custom_reward.py
@@ -185,7 +185,7 @@ def _models() -> dict[str, object]:
     if CONFIG.use_embedding and "embedder" not in _MODELS:
         from sentence_transformers import SentenceTransformer
 
-        _MODELS["embedder"] = SentenceTransformer("all-MiniLM-L6-v2")
+        _MODELS["embedder"] = SentenceTransformer("Alibaba-NLP/gte-multilingual-base")
     return _MODELS
 
 
